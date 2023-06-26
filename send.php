d91f7f832d3837a3189d731cd8549a0fa299144a
Dmitry <dmezhenskij@yandex.ru>
2023-06-26 16:13:53 +0300

    fixed bug mail php

--- send.php
+++ send.php
@@ -9,17 +9,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $content = $name . ' оставил заявку на обратный звонок. Номер телефона: ' . $number;
 
     $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers = 'Content-type: text/html; charset=utf-8' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 
-    $success = mail("dmezhenskij@yandex.ru", 'Запрос на обратный звонок СТО Антикор', $content, $headers);
-
-    //if ($success){
-    //    http_response_code(200);
-    //    echo "Письмо отправлено";
-    //} else {
-    //    http_response_code(500);
-    //    echo "Письмо не отправлено";
-    //}
+    mail("dmezhenskij@yandex.ru", 'Запрос на обратный звонок СТО Антикор', $content, $headers);
+    $success =
+    if ($success){
+        http_response_code(200);
+        echo "Письмо отправлено";
+    } else {
+        http_response_code(500);
+        echo "Письмо не отправлено";
+    }
 } else {
      http_response_code(403);
      echo "Данный метод запроса не поддерживается сервером";
