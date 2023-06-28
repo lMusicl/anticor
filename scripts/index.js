78710ad85e3ff2c28d6f50d29973334d52d8df87
Dmitry <dmezhenskij@yandex.ru>
2023-06-28 15:38:11 +0300

    already slider feedback

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,11 @@
 (function () {
     const StoAnti = {
-        feedbackPoint: null,
-        feedbackCard1: null,
-        feedbackCards: null,
-        feedbackCard2: null,
-        feedbackCard3: null,
-        feedbackCount: 1,
-
-
         burgerElement: null,
         closeElement: null,
         headerElement: null,
 
         init() {
 
-            this.feedbackSlider();
             this.gallerySlider();
             this.feedbackSlider();
             this.burger();
@@ -29,41 +20,12 @@
         },
         //Слайдер отзывов
         feedbackSlider() {
-            this.feedbackPoint = $('.slider__points__item1');
-            this.feedbackCards = $('.card__input');
-            this.feedbackCard1 = $('.card1').eq(0);
-            this.feedbackCard2 = $('.card2').eq(0);
-            this.feedbackCard3 = $('.card3').eq(0);
-
-            that = this;
-
-            this.feedbackCard1.click(() => {
-                this.feedbackPoint.eq(this.feedbackCount).removeClass("active__point");
-                this.feedbackCount = 0;
-                this.feedbackPoint.eq(this.feedbackCount).addClass("active__point");
-            })
-
-            this.feedbackCard2.click(() => {
-                this.feedbackPoint.eq(this.feedbackCount).removeClass("active__point");
-                this.feedbackCount = 1;
-                this.feedbackPoint.eq(this.feedbackCount).addClass("active__point");
-            })
-
-            this.feedbackCard3.click(() => {
-                this.feedbackPoint.eq(this.feedbackCount).removeClass("active__point");
-                this.feedbackCount = 2;
-                this.feedbackPoint.eq(this.feedbackCount).addClass("active__point");
-            })
-
-            for (let i = 0; i < this.feedbackPoint.length; i++) {
-                this.feedbackPoint[i].addEventListener("click", function () {
-                    that.feedbackCards.eq(that.feedbackCount).removeAttr('checked', 'checked');
-                    that.feedbackCount = i;
-                    that.feedbackCards.eq(that.feedbackCount).attr('checked', 'checked');
-                    that.feedbackPoint.removeClass("active__point");
-                    that.feedbackPoint.eq(i).addClass('active__point');
-                });
-            }
+            $('.feedback__slider').slick({
+                dots: true,
+                speed: 1000,
+                easing: 'easy',
+                centerMode: true,
+            });
         },
 
         //Слайдер фотографий
